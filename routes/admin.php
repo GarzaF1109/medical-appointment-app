387da95e74d65ed4bbfa42fab831ed474ebcdc3a
GarzaF1109 <fernandogarzamedina3@gmail.com>
2026-02-26 22:58:42 -0600

    vacio

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\RoleController;// Route::get('/', function () {
+use App\Http\Controllers\Admin\RoleController;// Route::get('/', function () 
 
 Route::get('/', function () {
     return view ('admin.dashboard');
